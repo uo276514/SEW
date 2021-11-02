fb2da4f8656de3d93cd1aa0d32f71b80400df6e8
uo276514 <90837405+uo276514@users.noreply.github.com>
2021-11-02 17:33:07 +0100

    Delete Cabecera.js

--- Cabecera.js
+++
@@ -1,8 +0,0 @@
-var asignatura = new Object();
-asignatura.nombre = "Software y estándares en la Web";
-asignatura.titulacion = "Grado en Ingeniería Informática del Software";
-asignatura.centro = "Escuela de Ingeniería Informática";
-asignatura.universidad = "Universidad de Oviedo";
-asignatura.curso = "2021-2022";
-asignatura.estudiante = "Jorge López Peláez";
-asignatura.email = "uo276514@uniovi.es";
