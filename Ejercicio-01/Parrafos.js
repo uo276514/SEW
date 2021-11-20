a4446400429254d0cf68bc116d210f4dd73a4b85
uo276514 <90837405+uo276514@users.noreply.github.com>
2021-11-20 19:50:59 +0100

    Delete Ejercicio-01 directory

--- Ejercicio-01/Parrafos.js
+++
@@ -1,9 +0,0 @@
-document.write("<p>Curso actual: ");
-document.write(asignatura.curso);
-document.write("</p>");
-document.write("<p>Nombre del estudiante: ");
-document.write(asignatura.estudiante);
-document.write("</p>");
-document.write("<p>Email del estudiante: ");
-document.write(asignatura.email);
-document.write("</p>");
